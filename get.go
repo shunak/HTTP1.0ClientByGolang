933ab0ccbdd2f5915311e4a91119a0cc12f974ab
DESKTOP-BRIGC4V\USER <shuna966@gmail.com>
2018-07-14 00:06:55 +0900

    180713

---
+++ get.go
@@ -0,0 +1,24 @@
+// http://localhost:18888に対してGETメソッドでアクセスし、ボディを取得する
+// $ curl http://localhost:18888をGo言語で実装
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://localhost:18888")
+	if err != nil { // Go言語のエラー処理コード　例外処理はない
+		panic(err)
+	}
+	defer resp.Body.Close() // 後処理コードdeferを付与すると、この関数から抜けたあとにこの文を実行する
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println(string(body))
+
+}
